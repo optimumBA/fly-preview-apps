580e9d96a32e86bc77cd71cc16a90b4d90d41f35
amos-kibet <kibetamos97@gmail.com>
2023-03-23 21:01:04 +0300

    refactor script

--- entrypoint.sh
+++ entrypoint.sh
@@ -18,7 +18,7 @@ EVENT_TYPE=$(jq -r .action /github/workflow/event.json)
 
 # Default the Fly app name to pr-{number}-{repo_name}
 app="${INPUT_NAME:-pr-$PR_NUMBER-$REPO_NAME}"
-app_db="${INPUT_NAME:-pr-$PR_NUMBER-$REPO_NAME}-db"
+app_db="$app-db"
 region="${INPUT_REGION:-${FLY_REGION:-iad}}"
 org="${INPUT_ORG:-${FLY_ORG:-personal}}"
 image="$INPUT_IMAGE"
@@ -32,71 +32,70 @@ fi
 # PR was closed - remove the Fly app if one exists and exit.
 if [ "$EVENT_TYPE" = "closed" ]; then
   flyctl apps destroy "$app" -y || true
+
   # destroy postgres db as well
-  flyctl apps destroy "$app_db" -y || true
-  exit 0
+  if flyctl status --app "$app_db"; then
+    flyctl apps destroy "$app_db" -y || true
+  fi
+  # destroy created volumes
+  # flyctl volumes destroy <id >-y || true
 fi
 
-# Deploy the Fly app, creating it first if needed.
+# Check if app exists,
+# if not, launch it, but don't deploy yet
 if ! flyctl status --app "$app"; then
   # Backup the original config file since 'flyctl launch' messes up the [build.args] section
   cp "$config" "$config.bak"
-  flyctl launch --no-deploy --copy-config --name "$app" --image "$image" --region "$region" --org "$org"
-  # set neccessary secrets
-  fly secrets set PHX_HOST="$app".fly.dev --app "$app"
+
+  flyctl launch --no-deploy --copy-config --name "$app" --region "$region" --org "$org"
+
   # Restore the original config file
   cp "$config.bak" "$config"
+
+  # look for "migrate" file in the app files
+  # if it exists, the app probably needs DB.
+  if [ -e "rel/overlays/bin/migrate" ]; then
+    # only create db if the app lauched successfully
+    if flyctl status --app "$APP"; then
+      flyctl postgres create --name "$app_db" --org "$org" --region "$region" --vm-size shared-cpu-1x --initial-cluster-size 1 --volume-size 10
+      # attach db to the app
+      flyctl postgres attach $"app_db" --app $"app"
+    fi
+  fi
+
+  # find a way to determine if the app requires volumes
+  # basically, scan the config file if it contains "[mounts]", then create a volume for it
+  # for now, we're just gonna create it anyway
+  #
+  # check if app name has dashes, and replace with underscore
+  # Fly.io does not accept dashes in volume names
+  if [[ "$app" =~ "-" ]]; then
+    volume=${app//-/_}
+  fi
+  while IFS= read -r LINE; do
+    if [[ $LINE == "[mounts]" ]]; then
+      fly volumes create "$volume" --app "$app" --region "$region"
+    fi
+  done <"$config"
 fi
 
-# find a way to create postgres for the app here👇
-# basically, look for "migrate" file in the app files
-# if it exists, the app probably needs DB.
-if stat rel/overlays/bin/migrate; then
-  flyctl postgres create --name "$app_db" --org "$org" --region "$region" --vm-size shared-cpu-1x --initial-cluster-size 1 --volume-size 10
-  # attaching db to the app
-  flyctl postgres attach "$app"
-  VOLUME=${PR_NUMBER//-/_}
-  fly volumes create "$VOLUME" --app "$app" --region "$region"
-fi
+# Trigger the deployment of the new version.
+echo "Contents of config $config file: " && cat "$config"
+flyctl deploy --config "$config" --app "$app" --region "$region" --strategy immediate
 
-# check if the app needs volumes, then create them
-# if [ grep -q \[mounts\] "$config" ]; then
-#   fly volumes create "$app" --app "$app" --region "$region"
-#   # 🎯 then, update the config, to have the newly created volume name
-# fi
-
-# Scale the VM before the deploy.
-# this is probably not needed at the moment
-# if [ -n "$INPUT_VM" ]; then
-#   flyctl scale --app "$app" vm "$INPUT_VM"
-# fi
-# if [ -n "$INPUT_MEMORY" ]; then
-#   flyctl scale --app "$app" memory "$INPUT_MEMORY"
-# fi
-# if [ -n "$INPUT_COUNT" ]; then
-#   flyctl scale --app "$app" count "$INPUT_COUNT"
-# fi
+# set neccessary secrets
+fly secrets set PHX_HOST="$app".fly.dev --app "$app"
 
 # import any environment secrets that may be required
 if [ -n "$INPUT_SECRETS" ]; then
   echo $INPUT_SECRETS | tr " " "\n" | flyctl secrets import --app "$app"
 fi
 
-# Attach postgres cluster to the app if specified.
-# if [ -n "$APP_NAME" ]; then
-# echo "<<==== attaching postgres to app $APP_NAME ====>>"
-#   flyctl postgres attach --app "$APP_NAME" || true
-# fi
-
-# Trigger the deploy of the new version.
-echo "Contents of config $config file: " && cat "$config"
-flyctl deploy --config "$config" --app "$app" --region "$region" --image "$image" --strategy immediate
-
 # Make some info available to the GitHub workflow.
 flyctl status --app "$app" --json >status.json
 hostname=$(jq -r .Hostname status.json)
 appid=$(jq -r .ID status.json)
-echo "hostname=$hostname" >> $GITHUB_OUTPUT
-echo "url=https://$hostname" >> $GITHUB_OUTPUT
-echo "id=$appid" >> $GITHUB_OUTPUT
-echo "name=$app" >> $GITHUB_OUTPUT
+echo "hostname=$hostname" >>$GITHUB_OUTPUT
+echo "url=https://$hostname" >>$GITHUB_OUTPUT
+echo "id=$appid" >>$GITHUB_OUTPUT
+echo "name=$app" >>$GITHUB_OUTPUT
