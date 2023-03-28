65ac4f00a031fd2d768f4b8251258f85e4b057c4
amos-kibet <kibetamos97@gmail.com>
2023-03-28 19:16:52 +0300

    refactor & debug action

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -l
+#!/bin/bash
 
 set -ex
 
@@ -17,62 +17,59 @@ REPO_NAME=$(echo $GITHUB_REPOSITORY | tr "/" "-")
 EVENT_TYPE=$(jq -r .action /github/workflow/event.json)
 
 # Default the Fly app name to pr-{number}-{repo_name}
-app="${INPUT_NAME:-pr-$PR_NUMBER-$REPO_NAME}"
-app_db="$app-db"
-region="${INPUT_REGION:-${FLY_REGION:-iad}}"
-org="${INPUT_ORG:-${FLY_ORG:-personal}}"
-image="$INPUT_IMAGE"
-config="${INPUT_CONFIG:-fly.toml}"
+APP="${INPUT_NAME:-pr-$PR_NUMBER-$REPO_NAME}"
+APP_DB="$APP-db"
+REGION="${INPUT_REGION:-${FLY_REGION:-iad}}"
+ORG="${INPUT_ORG:-${FLY_ORG:-personal}}"
+IMAGE="$INPUT_IMAGE"
+CONFIG="${INPUT_CONFIG:-fly.toml}"
 
-if ! echo "$app" | grep "$PR_NUMBER"; then
+if ! echo "$APP" | grep "$PR_NUMBER"; then
   echo "For safety, this action requires the app's name to contain the PR number."
   exit 1
 fi
 
 # PR was closed - remove the Fly app if one exists and exit.
 if [ "$EVENT_TYPE" = "closed" ]; then
-  flyctl apps destroy "$app" -y || true
+  flyctl apps destroy "$APP" -y || true
 
   # destroy postgres db as well
-  if flyctl status --app "$app_db"; then
-    flyctl apps destroy "$app_db" -y || true
+  if flyctl status --app "$APP_DB"; then
+    flyctl apps destroy "$APP_DB" -y || true
   fi
   # destroy created volumes
   # flyctl volumes destroy <id> -y || true
   exit 0
 fi
 
+# Backup the original config file since 'flyctl launch' messes up the [build.args] section
+# also, sources value under mounts is modified, for apps that require volumes
+cp "$config" "$config.bak"
+
 # Check if app exists,
 # if not, launch it, but don't deploy yet
-if ! flyctl status --app "$app"; then
-  # Backup the original config file since 'flyctl launch' messes up the [build.args] section
-  cp "$config" "$config.bak"
+if ! flyctl status --app "$APP"; then
 
-  echo "|> creating app ====>>"
-  flyctl launch --no-deploy --copy-config --name "$app" --region "$region" --org "$org"
-  echo "|> app created successfully ====>>"
-
-  sleep 2
-
-  # Restore the original config file
-  cp "$config.bak" "$config"
+  echo "|> creating $APP ====>>"
+  flyctl launch --no-deploy --copy-config --name "$APP" --region "$REGION" --org "$ORG"
+  echo "|> $APP created successfully ====>>"
 fi
 
 # look for "migrate" file in the app files
 # if it exists, the app probably needs DB.
 if [ -e "rel/overlays/bin/migrate" ]; then
   # only create db if the app lauched successfully
-  if flyctl status --app "$app"; then
-    if flyctl status --app "$app_db"; then
-      echo "$app_db already exists"
+  if flyctl status --app "$APP"; then
+    if flyctl status --app "$APP_DB"; then
+      echo "$APP_DB already exists"
     else
-      echo "|> creating DB ====>>"
-      flyctl postgres create --name "$app_db" --org "$org" --region "$region" --vm-size shared-cpu-1x --initial-cluster-size 1 --volume-size 10
-      echo "|> DB created successfully ====>>"
+      echo "|> creating $APP_DB DB ====>>"
+      flyctl postgres create --name "$APP_DB" --org "$ORG" --region "$REGION" --vm-size shared-cpu-1x --initial-cluster-size 1 --volume-size 10
+      echo "|> $APP_ID DB created successfully ====>>"
     fi
     # attaching db to the app if it was created successfully
     echo "|> attaching DB ====>>"
-    if $(flyctl postgres attach "$app_db" --app "$app" -y); then
+    if $(flyctl postgres attach "$APP_DB" --app "$APP" -y); then
       echo "|> DB attached ====>>"
     else
       echo "|> error attaching DB to app ====>>"
@@ -84,42 +81,46 @@ fi
 # basically, scan the config file if it contains "[mounts]", then create a volume for it
 # for now, we're just gonna create it anyway
 #
-# check if app name has dashes, and replace with underscore
+# replace any dash with underscore in app name
 # Fly.io does not accept dashes in volume names
-# if [[ "$app" =~ "-" ]]; then
-#   volume=${app//-/_}
-# fi
+VOLUME=$(echo $APP | tr '-' '_')
 
-if flyctl status --app "$app"; then
-  while IFS= read -r line; do
-    if [[ $line == "[mounts]" ]]; then
-      echo "|> creating volume ====>>"
-      fly volumes create temporary_volume --app "$app" --region "$region" --size 1
-      echo "|> volume created successfully ====>>"
-    fi
-  done <"$config"
+if grep -q "\[mounts\]" fly.toml; then
+  echo "|> creating volume ====>>"
+  fly volumes create $VOLUME --app "$APP" --region "$REGION" --size 10
+  echo "|> volume created successfully ====>>"
+
+  # modify config file to have the volume name specified above.
+  echo "|> updating config to contain new volume name ====>>"
+
+  # modify config file to have the volume name specified above.
+  sed -i -e 's/source =.*/source = '\"$VOLUME\"'/' "$CONFIG"
+  echo "|> config modified to contain new volume name ====>>"
 fi
 
 # Deploy the app.
 echo "|> deploying app ====>>"
-flyctl deploy --config "$config" --app "$app" --region "$region" --strategy immediate
+flyctl deploy --config "$CONFIG" --app "$APP" --region "$REGION" --strategy immediate
 echo "|> app deployed successfuly ====>>"
 
 # set neccessary secrets
 echo "|> setting secrets ====>>"
-fly secrets set PHX_HOST="$app".fly.dev --app "$app"
+fly secrets set PHX_HOST="$APP".fly.dev --app "$APP"
 echo "|> secrets set successfully ====>>"
 
+# Restore the original config file
+cp "$config.bak" "$config"
+
 # import any environment secrets that may be required
-# if [ -n "$INPUT_SECRETS" ]; then
-#   echo $INPUT_SECRETS | tr " " "\n" | flyctl secrets import --app "$app"
-# fi
+if [ -n "$INPUT_SECRETS" ]; then
+  echo $INPUT_SECRETS | tr " " "\n" | flyctl secrets import --app "$APP"
+fi
 
 # Make some info available to the GitHub workflow.
-flyctl status --app "$app" --json >status.json
-hostname=$(jq -r .Hostname status.json)
-appid=$(jq -r .ID status.json)
-echo "hostname=$hostname" >>$GITHUB_OUTPUT
-echo "url=https://$hostname" >>$GITHUB_OUTPUT
-echo "id=$appid" >>$GITHUB_OUTPUT
-echo "name=$app" >>$GITHUB_OUTPUT
+flyctl status --app "$APP" --json >status.json
+HOSTNAME=$(jq -r .Hostname status.json)
+APPID=$(jq -r .ID status.json)
+echo "hostname=$HOSTNAME" >>$GITHUB_OUTPUT
+echo "url=https://$HOSTNAME" >>$GITHUB_OUTPUT
+echo "id=$APPID" >>$GITHUB_OUTPUT
+echo "name=$APP" >>$GITHUB_OUTPUT
