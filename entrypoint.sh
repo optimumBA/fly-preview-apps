a98c9368d67c5d449a897a0c4a0a93d73bfed395
amos-kibet <kibetamos97@gmail.com>
2023-03-30 14:45:23 +0300

    refactor & debug action

--- entrypoint.sh
+++ entrypoint.sh
@@ -33,24 +33,18 @@ fi
 if [ "$EVENT_TYPE" = "closed" ]; then
   # destroy app DB
   if flyctl status --app "$APP_DB"; then
-    echo "|> destroying $APP_DB ====>>"
     flyctl apps destroy "$APP_DB" -y || true
-    echo "|> $APP_DB destroyed successfully ====>>"
   fi
 
   # destroy associated volumes as well
   if flyctl volumes list --app "$APP"; then
     VOLUME_ID=$(flyctl volumes list --app "$APP" | grep -oh "\w*vol_\w*")
-    echo "|> destroying $APP_DB volumes ====>>"
     flyctl apps destroy "$VOLUME_ID" -y || true
-    echo "|> $APP_DB destroyed successfully ====>>"
   fi
 
   # finally, destroy the app
   if flyctl status --app "$APP"; then
-    echo "|> destroying $APP ====>>"
     flyctl apps destroy "$APP" -y || true
-    echo "|> $APP destroyed successfully====>>"
   fi
   exit 0
 fi
@@ -62,10 +56,7 @@ cp "$CONFIG" "$CONFIG.bak"
 # Check if app exists,
 # if not, launch it, but don't deploy yet
 if ! flyctl status --app "$APP"; then
-
-  echo "|> creating $APP app ====>>"
   flyctl launch --no-deploy --copy-config --name "$APP" --region "$REGION" --org "$ORG"
-  echo "|> $APP app created successfully ====>>"
 fi
 
 # look for "migrate" file in the app files
@@ -76,16 +67,13 @@ if [ -e "rel/overlays/bin/migrate" ]; then
     if flyctl status --app "$APP_DB"; then
       echo "$APP_DB DB already exists"
     else
-      echo "|> creating $APP_DB DB ====>>"
-      flyctl postgres create --name "$APP_DB" --org "$ORG" --region "$REGION" --vm-size shared-cpu-1x --initial-cluster-size 4 --volume-size 10
-      echo "|> $APP_DB DB created successfully ====>>"
+      flyctl postgres create --name "$APP_DB" --org "$ORG" --region "$REGION" --vm-size shared-cpu-1x --initial-cluster-size 4 --volume-size 1
     fi
     # attaching db to the app if it was created successfully
-    echo "|> attaching $APP_DB DB ====>>"
     if flyctl postgres attach "$APP_DB" --app "$APP" -y; then
-      echo "|> $APP_DB DB attached ====>>"
+      echo "$APP_DB DB attached"
     else
-      echo "|> error attaching $APP_DB to $APP, attachments exist ====>>"
+      echo "Error attaching $APP_DB to $APP, attachments exist"
     fi
   fi
 fi
@@ -101,28 +89,22 @@ VOLUME=$(echo $APP | tr '-' '_')
 if grep -q "\[mounts\]" fly.toml; then
   # create volume only if none exists
   if ! flyctl volumes list --app "$APP" | grep -oh "\w*vol_\w*"; then
-    echo "|> creating $VOLUME volume ====>>"
-    flyctl volumes create "$VOLUME" --app "$APP" --region "$REGION" --size 10 -y
-    echo "|> $VOLUME volume created successfully ====>>"
+    flyctl volumes create "$VOLUME" --app "$APP" --region "$REGION" --size 1 -y
 
     # modify config file to have the volume name specified above.
-    echo "|> updating config to contain new volume name ====>>"
+    echo "Updating config to contain new volume name"
 
     # modify config file to have the volume name specified above.
     sed -i -e 's/source =.*/source = '\"$VOLUME\"'/' "$CONFIG"
-    echo "|> config modified to contain new volume name ====>>"
+    echo "Config file modified to contain new volume name"
   fi
 fi
 
 # Deploy the app.
-echo "|> deploying $APP app ====>>"
 flyctl deploy --config "$CONFIG" --app "$APP" --region "$REGION" --strategy immediate
-echo "|> $APP app deployed successfuly ====>>"
 
 # set neccessary secrets
-echo "|> setting secrets ====>>"
 fly secrets set PHX_HOST="$APP".fly.dev --app "$APP"
-echo "|> secrets set successfully ====>>"
 
 # import any environment secrets that may be required
 if [ -n "$INPUT_SECRETS" ]; then
@@ -130,6 +112,7 @@ if [ -n "$INPUT_SECRETS" ]; then
 fi
 
 # Restore the original config file
+echo "Restore the original config file"
 cp "$CONFIG.bak" "$CONFIG"
 
 # Make some info available to the GitHub workflow.
